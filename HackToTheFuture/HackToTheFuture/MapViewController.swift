ed08e81c9acfb1b2db87b90eeab4a5e6b8334867
Dominic Frazer Imregh <dominic.frazer@gmail.com>
2017-05-17 17:09:01 +0100

    Smooth slide and AR button added

--- HackToTheFuture/HackToTheFuture/MapViewController.swift
+++ HackToTheFuture/HackToTheFuture/MapViewController.swift
@@ -7,29 +7,31 @@
 //
 
 import UIKit
+import MapKit
 
 class MapViewController: BaseViewController {
 
+    @IBOutlet weak var mapView : MKMapView!
+    @IBOutlet weak var buttonAR : UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        buttonAR.layer.cornerRadius = buttonAR.frame.size.width / 2
+        buttonAR.clipsToBounds = true
+        buttonAR.layer.borderWidth = 2.0
+        buttonAR.layer.borderColor = UIColor.darkGray.cgColor
+        
+        mapView.showsUserLocation = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func didPressAR() {
+        
     }
-    */
 
 }
