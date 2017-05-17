69c9cae2108dd15b0a9074dec8abe9c80456d92d
Tushar Singhal <tsinghal@sapient.com>
2017-05-17 18:36:30 +0100

    Added pointers on map

---
+++ HackToTheFuture/HackToTheFuture/ARBaseViewController.swift
@@ -0,0 +1,135 @@
+//
+//  ARViewController.swift
+//  HackToTheFuture
+//
+//  Created by Tushar Singhal on 5/17/17.
+//  Copyright © 2017 HackToTheFuture. All rights reserved.
+//
+
+
+import CoreLocation
+import MapKit
+
+class ARBaseViewController: BaseViewController {
+    
+    fileprivate var places = [Place]()
+    fileprivate let locationManager = CLLocationManager()
+    @IBOutlet weak var mapView: MKMapView!
+    var arViewController: ARViewController!
+    var startedLoadingPOIs = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        locationManager.requestWhenInUseAuthorization()
+        mapView.userTrackingMode = MKUserTrackingMode.followWithHeading
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func showARController(_ sender: Any) {
+        arViewController = ARViewController()
+        arViewController.dataSource = self
+        arViewController.maxDistance = 0
+        arViewController.maxVisibleAnnotations = 30
+        arViewController.maxVerticalLevel = 5
+        arViewController.headingSmoothingFactor = 0.05
+        
+        arViewController.trackingManager.userDistanceFilter = 25
+        arViewController.trackingManager.reloadDistanceFilter = 75
+        arViewController.setAnnotations(places)
+        arViewController.uiOptions.debugEnabled = false
+        arViewController.uiOptions.closeButtonEnabled = true
+        
+        self.present(arViewController, animated: true, completion: nil)
+    }
+    
+    func showInfoView(forPlace place: Place) {
+        let alert = UIAlertController(title: place.placeName , message: place.infoText, preferredStyle: UIAlertControllerStyle.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: nil))
+        
+        arViewController.present(alert, animated: true, completion: nil)
+    }
+}
+
+extension ARBaseViewController: CLLocationManagerDelegate {
+    func locationManagerShouldDisplayHeadingCalibration(_ manager: CLLocationManager) -> Bool {
+        return true
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        
+        if locations.count > 0 {
+            let location = locations.last!
+            if location.horizontalAccuracy < 100 {
+                manager.stopUpdatingLocation()
+                let span = MKCoordinateSpan(latitudeDelta: 0.014, longitudeDelta: 0.014)
+                let region = MKCoordinateRegion(center: location.coordinate, span: span)
+                mapView.region = region
+                
+                if !startedLoadingPOIs {
+                    startedLoadingPOIs = true
+                    let loader = PlacesLoader()
+                    loader.loadPOIS(location: location, radius: 1000) { placesDict, error in
+                        if let dict = placesDict {
+                            guard let placesArray = dict.object(forKey: "results") as? [NSDictionary]  else { return }
+                            
+                            for placeDict in placesArray {
+                                let latitude = placeDict.value(forKeyPath: "geometry.location.lat") as! CLLocationDegrees
+                                let longitude = placeDict.value(forKeyPath: "geometry.location.lng") as! CLLocationDegrees
+                                let reference = placeDict.object(forKey: "reference") as! String
+                                let name = placeDict.object(forKey: "name") as! String
+                                let address = placeDict.object(forKey: "vicinity") as! String
+                                
+                                let location = CLLocation(latitude: latitude, longitude: longitude)
+                                let place = Place(location: location, reference: reference, name: name, address: address)
+                                
+                                self.places.append(place)
+                                let annotation = PlaceAnnotation(location: place.location!.coordinate, title: place.placeName)
+                                DispatchQueue.main.async {
+                                    self.mapView.addAnnotation(annotation)
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+extension ARBaseViewController: ARDataSource {
+    func ar(_ arViewController: ARViewController, viewForAnnotation: ARAnnotation) -> ARAnnotationView {
+        let annotationView = AnnotationView()
+        annotationView.annotation = viewForAnnotation
+        annotationView.delegate = self
+        annotationView.frame = CGRect(x: 0, y: 0, width: 150, height: 50)
+        
+        return annotationView
+    }
+}
+
+extension ARBaseViewController: AnnotationViewDelegate {
+    func didTouch(annotationView: AnnotationView) {
+        if let annotation = annotationView.annotation as? Place {
+            let placesLoader = PlacesLoader()
+            placesLoader.loadDetailInformation(forPlace: annotation) { resultDict, error in
+                
+                if let infoDict = resultDict?.object(forKey: "result") as? NSDictionary {
+                    annotation.phoneNumber = infoDict.object(forKey: "formatted_phone_number") as? String
+                    annotation.website = infoDict.object(forKey: "website") as? String
+                    
+                    self.showInfoView(forPlace: annotation)
+                }
+            }
+            
+        }
+    }
+}
